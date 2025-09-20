43604eabb77197a9c03493227229a919d078d07f
Anyudat <balint22222@gmail.com>
2025-09-20 17:36:04 +0200

    first-layout

--- Database.php
+++ Database.php
@@ -7,24 +7,39 @@ class Database{
     private $password = "";
     private $DB;
 
+    // fields to upload files
+    private $targetFile;
+    private $imageFileType;
+
     function __construct(){
         $this->createDatabase($this->dbName);
+
+        $this->checkFileUpload();
+
+        $this->DB->close();
     }
+
+    function checkFileUpload(){
+        if (isset($_POST["submit"])){
+            $this->targetFile = basename($_FILES["fileUpload"]["name"]);
+            $this->imageFileType = strtolower(pathinfo($this->targetFile, PATHINFO_EXTENSION));
+            echo $this->imageFileType;
+        }
+    }
+
+    
 
     function getConn($dbName)
     {
         try {
-            // Kapcsolódás az adatbázishoz
             $mysqli = mysqli_connect($this->host, $this->user, $this->password, $dbName);
 
-            // Ellenőrizzük a csatlakozás sikerességét
             if (!$mysqli) {
                 throw new Exception("Kapcsolódási hiba az adatbázishoz: " . mysqli_connect_error());
             }
 
             return $mysqli;
         } catch (Exception $e) {
-            // Hibás csatlakozás esetén `null`-t ad vissza
             return null;
         }
     }
@@ -32,16 +47,16 @@ class Database{
     function dbExists()
     {
         try {
-            $mysqli = $this->getConn('mysql');
-            if (!$mysqli) {
+            $this->DB = $this->getConn('mysql');
+            if (!$this->DB) {
                 return false;
             }
 
             $query = sprintf("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '%s';", $this->dbName);
-            $result = $mysqli->query($query);
+            $result = $this->DB->query($query);
 
             if (!$result) {
-                throw new Exception('Lekérdezési hiba: ' . $mysqli->error);
+                throw new Exception('Lekérdezési hiba: ' . $this->DB->error);
             }
             $exists = $result->num_rows > 0;
 
@@ -51,26 +66,31 @@ class Database{
         catch (Exception $e) {
             return false;
         }
-        finally {
-            // Ensure the database connection is always closed
-            $mysqli?->close();
-        }
+    }
 
+    function uploadToTable($tableName, $fields, $data){
+        $this->DB->query("INSERT INTO `{$this->dbName}`{$tableName}({$fields}) VALUES($data)");
     }
 
     function createDatabase(){
         if (!$this->dbExists()){
-            $this->DB = $this->getConn("mysql");
             $this->DB->query("CREATE DATABASE {$this->dbName} CHARACTER SET utf8 COLLATE utf8_general_ci;");
+
+            $this->DB->close();
             $this->DB = $this->getConn($this->dbName);
+
             $this->createTables();
+            $this->fillTables();
         }
     }
 
     function createTables(){
-        $this->DB->query("CREATE TABLE songs(id INT NOT NULL PRIMARY KEY, artist_id INT NOT NULL, album_id INT, genre VARCHAR(100), `language` VARCHAR(100));");
-        $this->DB->query("CREATE TABLE albums(id INT NOT NULL PRIMARY KEY, name VARCHAR(100), instrument VARCHAR(100), image_location VARCHAR(100));");
-        $this->DB->query("CREATE TABLE artists(id INT NOT NULL PRIMARY KEY, name VARCHAR(100) NOT NULL, album_cover VARCHAR(100), release_date DATE);");
+        $this->DB->query("CREATE TABLE songs(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, artist_id INT NOT NULL, album_id INT, title VARCHAR(100), genre VARCHAR(100), `language` VARCHAR(100));");
+        $this->DB->query("CREATE TABLE artists(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100), instrument VARCHAR(100), image MEDIUMBLOB);");
+        $this->DB->query("CREATE TABLE albums(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100) NOT NULL, album_cover MEDIUMBLOB, release_date INT);");
     }
 
+    function fillTables(){
+
+    }
 }
